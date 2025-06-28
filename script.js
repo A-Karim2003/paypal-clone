6b5288d7d4b86d95e0bd8f0421570345d6693e90
Abdoulkarim <17a.karim@gmail.com>
2025-06-28 22:02:28 +0100

    feat: add collapsible sidebar navigation with dropdowns for Personal and Business sections

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const navItemContainer = document.querySelectorAll(".nav-item-container");
 
 const header = document.querySelector("header");
+const hambugerMenu = document.querySelector(".menu-container");
+const menuIcon = document.querySelector(".menu-container i");
+const sidebar = document.querySelector("aside");
+
+const sidebarNavContainer = document.querySelectorAll(".sidebar-nav-container");
 
 //* show drop down once nav item is hovered
 navItemContainer.forEach((navItem) => {
@@ -18,5 +23,62 @@ navItemContainer.forEach((navItem) => {
     const dropDown = navItem.querySelector(".drop-down");
     dropDown.classList.remove("visible");
     header.style.background = "transparent";
+  });
+});
+
+// ------------------------------------------------//
+
+//* Hambuger/sidebar section
+
+hambugerMenu.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+
+  if (sidebar.classList.contains("active")) {
+    hambugerMenu.classList.add("active");
+    menuIcon.classList.replace("fa-bars", "fa-x");
+  } else {
+    hambugerMenu.classList.remove("active");
+    menuIcon.classList.replace("fa-x", "fa-bars");
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (sidebar.classList.contains("active")) {
+    hambugerMenu.classList.add("active");
+    menuIcon.classList.replace("fa-bars", "fa-x");
+  } else {
+    hambugerMenu.classList.remove("active");
+    menuIcon.classList.replace("fa-x", "fa-bars");
+  }
+});
+
+//* removes active classes once page is reloaded
+
+window.addEventListener("load", () => {
+  const navType = performance.getEntriesByType("navigation")[0].type;
+
+  if (navType === "reload") {
+    hambugerMenu.classList.remove("active");
+    menuIcon.classList.replace("fa-x", "fa-bars");
+    hambugerMenu.classList.remove("active");
+  }
+});
+
+// ------------------sidebar content section------------//
+
+sidebarNavContainer.forEach((sidebarNav) => {
+  sidebarNav.addEventListener("click", () => {
+    const dropdown = sidebar.querySelector(".sidebar-dropdown");
+    //? Make inner sidebar show
+    dropdown.classList.add("active");
+  });
+});
+
+const dropdowns = document.querySelectorAll(".sidebar-dropdown");
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdown.classList.remove("active");
   });
 });
